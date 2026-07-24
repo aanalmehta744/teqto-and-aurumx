9be1fb3ab4b62c519240c0c08641c08fd5e6d86e
aanalmehta744 <aanalmehta744@gmail.com>
2026-07-24 15:25:27 +0530

    Update API URL to Render backend

--- source/main/src/environments/environment.ts
+++ source/main/src/environments/environment.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://portal.eliteinfotec.in/api'
+  // apiUrl: 'https://portal.eliteinfotec.in/api'
+  // apiUrl: 'http://localhost:3000/api'
+  apiUrl: 'https://teqto-and-aurumx.onrender.com/api'
 };
 console.log("Production Enviorment Loaded. API URL", environment.apiUrl);
