36fc70f56a95017dfac34fad6d7506b2c25e7839
Maneeka03 <joshineeharika981@gmail.com>
2026-08-20 17:25:52 +0530

    fixed dashboard welcome card

--- source/main/src/environments/environment.development.ts
+++ source/main/src/environments/environment.development.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
-  localApiUrl: 'http://localhost:3000/api',
+  localApiUrl: 'http://localhost:5001/api',
 
   // cloudflareApiUrl:
   //   'https://temp-acrylic-velvet-lynn.trycloudflare.com/api',
 
   apiUrl:
-    'http://localhost:3000/api'
+    'http://localhost:5001/api'
 };
 
 console.log(
